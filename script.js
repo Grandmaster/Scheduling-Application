a65ea6b9d17e1c89d776e0494f18e1588f7308c0
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-13 01:25:31 -0400

    Added code that prints date in desired format to browser window

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Code for the scheduling application.
 // =================================================
 
+$(document).ready(function(){
+    // Gets date from moment object and prints value on browser window
+    var date = moment().format('dddd, MMMM Do');
+    $('#currentDay').text(date);
+})
