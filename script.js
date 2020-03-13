b0347eb3007433d3d0ee499b724bde67bf562861
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-13 10:08:22 -0400

    Fixed layout for index.html

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Code for the scheduling application.
 // =================================================
 
-$(document).ready(function(){
-    // Gets date from moment object and prints value on browser window
-    var date = moment().format('dddd, MMMM Do');
-    $('#currentDay').text(date);
-})
+$(document).ready(function() {
+  // Gets date from moment object and prints value on browser window
+  var date = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(date);
+  // Styles columns by comparing actual time to printed value in timeblock
+  var hour = moment().format("LT");
+  var formattedHour = hour.replace(/:\d+ /, "");
+  console.log(formattedHour);
+});
