2d6facd4da1553afac36e618e3771164fa217c92
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-13 15:02:30 -0400

    Added code that styles the columns no matter what time it is

--- script.js
+++ script.js
@@ -18,30 +18,54 @@ $(document).ready(function() {
     "5PM": ""
   };
   // Grabs current time and formats to match strings in timeblock
-  var hour = moment().format("LT");
+  //   var hour = moment().format("LT");
+  var hour = "8:32 AM";
   var formattedHour = hour.replace(/:\d+ /, "");
+  var middayTag = hour.replace(/\d+:\d+ /, "");
   console.log(formattedHour);
-  // Styles columns based on time data
-  var array = [];
+  console.log("middayTag: " + middayTag);
+  // Styles columns based on time data. If the time is not on the list of timeblocks,
+  // it styles the columns based on what time it is (they are either all in the future
+  // or all in the past).
+  var hourConditionalsList = [];
   for (let i of Object.keys(eventStorage)) {
     if (i == formattedHour) {
-      array.push(true);
-    } else array.push(false);
+      hourConditionalsList.push(true);
+    } else hourConditionalsList.push(false);
   }
-  var currentHour = array.indexOf(true);
-  $(".hour").each(function(index) {
-    if (index < currentHour) {
-      $(this)
-        .next()
-        .addClass("past");
-    } else if (index == currentHour) {
-      $(this)
-        .next()
-        .addClass("present");
-    } else if (index > currentHour) {
+  var hourCheck = hourConditionalsList.filter(a => {
+    return a == true;
+  });
+  console.log(hourCheck);
+  if (hourCheck.length == 0 && middayTag == "AM") {
+    $(".hour").each(function() {
       $(this)
         .next()
         .addClass("future");
-    }
-  });
+    });
+  } else if (hourCheck.length == 0 && middayTag == "PM") {
+    $(".hour").each(function() {
+      $(this)
+        .next()
+        .addClass("past");
+    });
+  } else if (hourCheck.length !== 0) {
+    var currentHour = hourConditionalsList.indexOf(true);
+    console.log(currentHour);
+    $(".hour").each(function(index) {
+      if (index < currentHour) {
+        $(this)
+          .next()
+          .addClass("past");
+      } else if (index == currentHour) {
+        $(this)
+          .next()
+          .addClass("present");
+      } else if (index > currentHour) {
+        $(this)
+          .next()
+          .addClass("future");
+      }
+    });
+  }
 });
