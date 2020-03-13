e3963648ae973e6316e2002d44486e1c98b80585
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-13 11:24:19 -0400

    Added object that represents application to store in local storage

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ $(document).ready(function() {
   var hour = moment().format("LT");
   var formattedHour = hour.replace(/:\d+ /, "");
   console.log(formattedHour);
+
+  // Creates object to store in localStorage
+  var eventStorage = {
+    "9AM": "",
+    "10AM": "",
+    "11AM": "",
+    "12PM": "",
+    "1PM": "",
+    "2PM": "",
+    "3PM": "",
+    "4PM": "",
+    "5PM": ""
+  };
 });
