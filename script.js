6cc8e6b7e4a0cf8e31a05ae27cfb8cdd8f7a8bd6
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-13 20:50:36 -0400

    Put in code that saves textarea value to local Storage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // =================================================
 
 $(document).ready(function() {
+  // Loads saved time data from localStorage, and prints it to screen
+  scheduleData = JSON.parse(localStorage.getItem("Schedule"));
   // Gets date from moment object and prints value on browser window
   var date = moment().format("dddd, MMMM Do");
   $("#currentDay").text(date);
@@ -19,11 +21,9 @@ $(document).ready(function() {
   };
   // Grabs current time and formats to match strings in timeblock
   //   var hour = moment().format("LT");
-  var hour = "8:32 AM";
+  var hour = "2:33 PM";
   var formattedHour = hour.replace(/:\d+ /, "");
   var middayTag = hour.replace(/\d+:\d+ /, "");
-  console.log(formattedHour);
-  console.log("middayTag: " + middayTag);
   // Styles columns based on time data. If the time is not on the list of timeblocks,
   // it styles the columns based on what time it is (they are either all in the future
   // or all in the past).
@@ -36,7 +36,6 @@ $(document).ready(function() {
   var hourCheck = hourConditionalsList.filter(a => {
     return a == true;
   });
-  console.log(hourCheck);
   if (hourCheck.length == 0 && middayTag == "AM") {
     $(".hour").each(function() {
       $(this)
@@ -51,7 +50,6 @@ $(document).ready(function() {
     });
   } else if (hourCheck.length !== 0) {
     var currentHour = hourConditionalsList.indexOf(true);
-    console.log(currentHour);
     $(".hour").each(function(index) {
       if (index < currentHour) {
         $(this)
@@ -68,4 +66,15 @@ $(document).ready(function() {
       }
     });
   }
+  // Saves data in textarea field to localStorage when corresponding save icon is clicked.
+  $(".saveBtn").on("click", function() {
+    var correspondingHour = $(this)
+      .prev()
+      .prev()
+      .html();
+    eventStorage[correspondingHour] = $(this)
+      .prev()
+      .val();
+    localStorage.setItem("Schedule", JSON.stringify(eventStorage));
+  });
 });
